d39a736350a2541f73e6ce1a724772dbdd168b5b
Upper <goodp@yandex.ru>
2022-05-06 20:47:28 +0300

    Exercise 3

---
+++ Desktop/Hometask c#/Exercise3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a number: ");
+int number= int.Parse(Console.ReadLine());
+
+if ((number%2)==0)
+{
+   Console.WriteLine("It is even number");
+}
+else
+{
+    Console.WriteLine("It is odd number");
+} 
