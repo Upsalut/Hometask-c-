5e69a65aee2558a4df6e4b4a4245b6b7bc4d34ce
Upper <goodp@yandex.ru>
2022-05-07 01:43:01 +0300

    Exercise 8

---
+++ Desktop/Hometask c#/Exercise8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write a number: ");
+int number=int.Parse(Console.ReadLine());
+Console.WriteLine("Ok, we'll get a random number and check is that multiple for yours or no..");
+int random=new Random().Next(-10000,100000);
+Console.WriteLine(random);
+
+int multiple=random%number;
+
+if (multiple>0)
+Console.WriteLine("Random number is'nt multiple for yours, the remainder is: "+multiple);
+if (multiple<0)
+Console.WriteLine("Random number is'nt multiple for yours, the remainder is: "+multiple);
+
+if(multiple==0)
+Console.WriteLine("Random number is multiple for yours: "+random+"/"+number+"="+random/number);
