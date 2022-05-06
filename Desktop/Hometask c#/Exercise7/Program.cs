177014f14cff55e2e8e075e3c5b689004b324c77
Upper <goodp@yandex.ru>
2022-05-07 00:46:23 +0300

    Exercise 7

---
+++ Desktop/Hometask c#/Exercise7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Write a three-digit number: ");
+int number=int.Parse(Console.ReadLine());
+if (number<100)
+Console.WriteLine("Sorry, it is inappropriate number and your answer would be incorrect, write a three-digit number!");
+if (number>999)
+Console.WriteLine("Sorry, it is inappropriate number and your answer would be incorrect, write a three-digit number!");
+
+int digit1=number/100;
+digit1=digit1%10;
+int digit2=number/10;
+digit2=digit2%10;
+int digit3=number%10;
+
+Console.Write(digit1+""+digit3);
