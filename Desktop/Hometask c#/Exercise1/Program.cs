d3628725a2ae2e9045a18a265c3b6706f55381be
Upper <goodp@yandex.ru>
2022-05-06 05:11:45 +0300

    Exercise 1

---
+++ Desktop/Hometask c#/Exercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Write a number: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Write another one: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x==y*y)
+Console.Write("Digits checked and "+x+" is square of the number "+y);
+
+else
+Console.Write("Digits checked, there are no squares of number");
