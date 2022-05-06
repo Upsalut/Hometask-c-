5344dd72981ad7f4bcfed358cd44b1ddf41a7731
Upper <goodp@yandex.ru>
2022-05-06 22:11:25 +0300

    Exercise 6

---
+++ Desktop/Hometask c#/Exercise6/Program.cs
@@ -0,0 +1,11 @@
+﻿int number= new Random().Next(10,99);
+Console.WriteLine(number);
+int digit1=number/10;
+digit1=digit1%10;
+int digit2=number/1;
+digit2=digit2%10;
+
+if (digit1>digit2)
+Console.WriteLine(digit1+" is the biggest one");
+else
+Console.WriteLine(digit2+" is the biggest one");
