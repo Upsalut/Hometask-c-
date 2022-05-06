d5d29715da22d6888641bc42eb894af81a152c92
Upper <goodp@yandex.ru>
2022-05-06 20:14:17 +0300

    Exercise 2

---
+++ Desktop/Hometask c#/Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Write a number from 1 to 7: ");
+int day=int.Parse(Console.ReadLine());
+if (day>7)
+Console.Write("Sorry, it is inappropriate number, write number from 1 to 7!");
+if (day<1)
+Console.Write("Sorry, it is inappropriate number, write number from 1 to 7!");
+if (day==1)
+Console.WriteLine("Monday");
+if (day==2)
+Console.WriteLine("Tuesday");
+if (day==3)
+Console.WriteLine("Wendsday");
+if (day==4)
+Console.WriteLine("Thursday");
+if (day==5)
+Console.WriteLine("Friday");
+if (day==6)
+Console.WriteLine("Saturday");
+if (day==7)
+Console.WriteLine("Sunday");
+
+
+
