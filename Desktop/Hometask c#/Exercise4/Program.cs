a6012a242f039a0c3ad883d9aabddd0b76448f7b
Upper <goodp@yandex.ru>
2022-05-06 21:30:02 +0300

    Exercise 4

---
+++ Desktop/Hometask c#/Exercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Write a number from 2: ");
+int number= int.Parse(Console.ReadLine());
+if (number<1)
+Console.WriteLine("Sorry, it is inappropriate number, write a number from 2!");
+while (number>=2)
+{ 
+ if (number%2==0)
+   { 
+   Console.Write(number+" ");
+   number=number-1;
+   }
+ else
+   number=number-1;
+}
