efd6203f860d0f00a262d571ee696ceee0398b0b
Upper <goodp@yandex.ru>
2022-05-07 20:43:57 +0300

    Exercise 9

---
+++ Desktop/Hometask c#/Exercise9/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Write a three-digit number: ");
+int number= int.Parse(Console.ReadLine());
+int digit=number/1;
+if (number>=100)
+{
+    if (number<=999) 
+    digit=digit%10;
+    Console.WriteLine(digit);
+}
+if (number<100)
+Console.WriteLine("There is no third digit.");
+if (number>(-100))
+{
+    if (number<100)
+    Console.WriteLine("Let's try again.");
+}
+if (number>999)
+Console.WriteLine("Sorry, it is inappropriate number, write a three-digit number!");
+if (number<-999)
+Console.WriteLine("Sorry, it is inappropriate number, write a three-digit number!");
